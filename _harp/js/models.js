cba29795296e58b1c416c3c71071e86a2c2bd9d6
Khanh Nguyen <vu@Khanhs-MacBook-Pro.local>
2015-07-28 17:55:42 +1000

    Actually added the files

---
+++ _harp/js/models.js
@@ -0,0 +1,11 @@
+var $grid = $('.model-phoenix .p-masonry').packery({
+	// Masonry Options
+	itemSelector  : '.model-phoenix .p-element',
+	gutter 		  : 0
+});
+
+// $grid.click(
+// 	function() {
+// 		$grid.masonry('layout');
+// 	}
+// );
